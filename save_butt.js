7564ff48eb81609c6d8889a1d7ba47dbfcc617f8
mangotapioca13 <al71@rice.edu>
2019-03-02 13:44:23 -0600

    save button changes

--- save_butt.js
+++ save_butt.js
@@ -1,16 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-    var saveButton = document.getElementById('save_button');
+//     var saveButton = document.getElementById('save_button');
   
-    saveButton.addEventListener('click', function() {
-        // Find html elements.
-        var textArea = document.getElementById('my_text');
-        var div = document.getElementById('view_text');
+//     saveButton.addEventListener('click', function() {
+//         // Find html elements.
+//         var textArea = document.getElementById('my_text');
+//         var div = document.getElementById('view_text');
 
-        // Put the text in a variable so we can manipulate it.
-        var text = textArea.value;
+//         // Put the text in a variable so we can manipulate it.
+//         var text = textArea.value;
 
-        // Print the text in the div made for it.
-        div.innerHTML = text;
-    }, false);    
-  }, false);
+//         // Print the text in the div made for it.
+//         div.innerHTML = text;
+//     }, false);    
+//   }, false);
+
+window.onload = function() {
+    document.getElementById('save_button').onclick = function() {
+        var value = document.getElementById('my_text').value;
+
+        chrome.storage.sync.set({'myLine': value}, function(){
+            // alert('Success!');
+        });
+    }
+}
