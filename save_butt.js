7c079ad6bc3deae93177d6bf7fdd4b0f0a9a633b
mangotapioca13 <al71@rice.edu>
2019-03-02 14:31:47 -0600

    getting url

--- save_butt.js
+++ save_butt.js
@@ -15,18 +15,60 @@
 //     }, false);    
 //   }, false);
 
-window.onload = function() {
-    chrome.storage.sync.set({'myLine': ''});
-    chrome.storage.sync.get('myLine', function(data) {
-        var div = document.getElementById('view_text');
-        div.innerHTML = data.myLine;
+//get url
+function getCurrentTabUrl(callback) { 
+    var queryInfo = {
+      active: true,
+      currentWindow: true
+    };
+     chrome.tabs.query(queryInfo, function(tabs) {
+      var tab = tabs[0];
+      var url = tab.url;
+      callback(url);
+    });
+ }
+
+// function renderURL(statusText) {
+//     document.getElementById('urlArea').textContent = statusText;
+// }
+
+// function storeURL(statusText) {
+//     //return statusText;
+//     text = statusText;
+//     return text
+// }
+
+
+//get url button functionality
+// document.addEventListener('DOMContentLoaded', function() {
+//     var getUrl = document.getElementById('getUrl');
+
+//     getUrl.addEventListener('click', function() {
+//         getCurrentTabUrl(function(url) {
+//             storeURL(url);
+//         });
+//     }, false);
+// }, false);
+ 
+
+window.onload = function() {    
+    var box = document.getElementById('view_text');
+    var myline = getCurrentTabUrl(function(url) {
+        myline = url;
+    }); 
+
+    chrome.storage.sync.set({myline: ''});
+    chrome.storage.sync.get(myline, function(data) {
+        box.innerHTML = data.myline;
     });
 
     document.getElementById('save_button').onclick = function() {    
-        var value = document.getElementById('my_text').value;
-
-        chrome.storage.sync.set({'myLine': value}, function(){
-            // alert('Success!');
+        var note = document.getElementById('my_text').value;
+        note = note.replace(/\n/gi, "<br />");
+        chrome.storage.sync.set({myline: note}, function(data){
+            //alert('Success!');
+            alert(myline);
         });
+
     }
 }
